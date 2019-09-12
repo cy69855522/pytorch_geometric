609f91b856d33c167b059ebbaa518721b67d1a22
Knife丶 <1272068154@qq.com>
2019-09-12 20:59:33 +0800

    Update fixed_points.py

--- torch_geometric/transforms/fixed_points.py
+++ torch_geometric/transforms/fixed_points.py
@@ -1,7 +1,6 @@
     
 from __future__ import division
 
-import re
 import math
 
 import torch
@@ -27,15 +26,15 @@ class FixedPoints(object):
         num_nodes = data.num_nodes
 
         if self.replace:
-            choice = np.random.choice(data.num_nodes, self.num, replace=True)
+            choice = np.random.choice(num_nodes, self.num, replace=True)
         else:
             choice = torch.cat([
                 torch.randperm(num_nodes)
-                for _ in range(math.ceil(num_nodes / self.num))
+                for _ in range(math.ceil(self.num / num_nodes))
             ], dim=0)[:self.num]
 
         for key, item in data:
-            if bool(re.search('edge', key)):
+            if 'edge' in key:
                 continue
             if torch.is_tensor(item) and item.size(0) == num_nodes:
                 data[key] = item[choice]
